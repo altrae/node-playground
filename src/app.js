b96c861112635c0543b91f54c5e2830ba35c4704
altrae <altrae@users.noreply.github.com>
2023-01-11 11:14:55 -0600

    Read/write file streams in app

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const zlib = require("zlib");
 
 const read = (error, data) => {
   if (error) throw error;
@@ -8,12 +9,36 @@ const read = (error, data) => {
 
 const readme = fs.readFile("./readme.md", "utf-8", read);
 
+const readable = fs.createReadStream(`${__dirname}/assets/lorem-ipsum.txt`, {
+  encoding: "utf-8",
+  highWaterMark: 16 * 1024,
+});
+
+const writable = fs.createWriteStream(
+  `${__dirname}/assets/lorem-ipsum_copy.txt`
+);
+
+// readable.on("data", (chunk) => {
+//   console.log({ chunk });
+//   writable.write(chunk);
+// });
+
+// pipe does the same thing as listening for the data event and writing the chunks
+readable.pipe(writable);
+
+const compressed = fs.createWriteStream(
+  `${__dirname}/assets/lorem-ipsum-copy.txt.gz`
+);
+const gzip = zlib.createGzip();
+
+readable.pipe(gzip).pipe(compressed);
+
 exports = "a";
 module.exports = "b";
 
 console.log({
   exports,
-  module: { ...module.exports },
+  module: { exports: { ...module.exports } },
   equal: exports === module.exports,
   readme,
 });
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-// @ts-ignore - needed due to requirement that imports reference js, not ts, files
-import { ts } from "./ts-module.ts";
+import { ts } from "./ts-module.js";
 import fs from "node:fs/promises";
-// @ts-ignore - needed due to requirement that imports reference js, not ts, files
-import { EventEmitter } from "./event-emitter.ts";
-// import { EventEmitter } from "node:events";
+import { Zlib } from "node:zlib";
+import { EventEmitter } from "node:events";
+// import { EventEmitter } from "./event-emitter.js";
+import { __dirname, __filename } from "./utils.js";
+
+console.log({ __dirname, __filename });
 
 const readme = await fs.readFile("./readme.md", "utf-8");
 
@@ -18,15 +20,32 @@ const event2 = (event) => {
 };
 
 emitter.addListener("event", event1);
-
 emitter.addListener("event", event2);
 
 emitter.emit("event", { some: "event data" });
 
 emitter.removeListener("event", event1);
-
 emitter.removeListener("event", event2);
 
 emitter.emit("event", { some: "event data" });
 
-console.log({ emitter, readme, ts });
+const buffer = Buffer.from("Hello");
+
+const loremIpsum = fs.readFile(`${__dirname}/assets/lorem-ipsum.txt`, {
+  encoding: "utf-8",
+});
+
+loremIpsum.then((buffer) => {
+  // console.log(buffer.toString());
+  console.log({ loremIpsumBuffer: buffer.toString().length });
+});
+
+console.log({
+  __dirname,
+  buffer,
+  bufferJSON: buffer.toJSON(),
+  bufferString: buffer.toString(),
+  emitter,
+  readme,
+  ts,
+});
